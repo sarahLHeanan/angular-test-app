f3b089ed9a718a45cfe5efa0747cad53553f119c
Sarah Heanan <Sarah Heanan>
2016-05-22 16:48:01 +0100

    added add and remove button functionality

--- scripts/app.js
+++ scripts/app.js
@@ -8,10 +8,13 @@ app.controller("testController", ['$scope', function($scope) {
 	];
 
 	$scope.addToDo = function() {
-
+		$scope.todos.push({'item':$scope.newToDo, 'done':false});
+		$scope.newToDo = '';
 	};
 
 	$scope.removeToDo = function() {
-
+		$scope.todos = $scope.todos.filter(function(item) {
+			return !item.done;
+		})
 	};
 }])
